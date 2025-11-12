0981cddf01f0f825a4c54dd9b08300122ac8b116
kjmorse2 <kenmorse2@proton.me>
2025-11-12 11:01:58 -0700

    2D point class implemented, Walls recieved.

--- Snake/Snake.Client/Models/Point2D.cs
+++ Snake/Snake.Client/Models/Point2D.cs
@@ -1,5 +1,12 @@
-﻿namespace CS3500.Snake.Models;
+﻿using System.Text.Json.Serialization;
+
+namespace CS3500.Snake.Models;
 
 public class Point2D
 {
+    [JsonPropertyName("X")]
+    public int X { get; set; }
+
+    [JsonPropertyName("Y")]
+    public int Y { get; set; }
 }
--- Snake/Snake.Client/Pages/SnakeGame.razor.cs
+++ Snake/Snake.Client/Pages/SnakeGame.razor.cs
@@ -4,4 +4,12 @@ namespace CS3500.Snake.Client.Pages.SnakeGame;
 using CS3500.Snake.Models;
 public partial class SnakeGame
 {
+    public static void GameLoop(int playerId, World world)
+    {
+        new Thread(() =>
+        {
+            
+        }).Start();
+
+    }
 }
