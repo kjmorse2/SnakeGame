93cf1dc3c74fd3fd164047016c4827df8a90c838
Kenenth Morse <kenmorse2@proton.me>
2025-12-03 16:08:31 -0700

    Started on web server, different approach than Hunter

--- WebServer/Program.cs
+++ WebServer/Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+using System.Net;
+using CS3500.Networking;
 
-Console.WriteLine("Hello, World!");
+// TODO add logging.
+class Program
+{
+
+    private static NetworkConnection connection; 
+    static void Main(string[ ] args)
+    {
+        int port = 80;
+        HttpListener listener = new ();
+        listener.Prefixes.Add(string.Format("http://localhost:{0}/", port));
+
+        listener.Start();
+        Console.WriteLine("Starting web server on localhost, port " + port);
+
+        while (true)
+        {
+            HttpListenerContext context = listener.GetContext();
+            HttpListenerRequest request = context.Request;
+            HttpListenerResponse response = context.Response;
+
+            string responseString = "<html><body><h1>Welcome to the Snake Game Web Server!</h1></body></html>";
+
+        }
+    }
+}
