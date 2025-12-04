2a7b6e61c67501adce246644ad4e9f170154aa57
Hunter Simmons <huntersoccer28@gmail.com>
2025-12-04 12:46:15 -0700

    PlayerData and GameData documented.

--- Networking/Records/GameData.cs
+++ Networking/Records/GameData.cs
@@ -6,8 +6,18 @@ using System.Globalization;
 
 namespace CS3500.Networking.Records;
 
+/// <summary>
+/// Represents data associated with a specific game, including its unique identifier, start time, and end time.
+/// </summary>
+/// <remarks>This class is immutable. </remarks>
 public sealed class GameData
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GameData"/> class.
+    /// </summary>
+    /// <param name="gameId"> The ID of the game.</param>
+    /// <param name="startTime">The time when the game started.</param>
+    /// <param name="endTime">The time at when the game ended.</param>
     public GameData(int gameId, DateTime startTime, DateTime endTime)
     {
         GameId = gameId;
@@ -15,12 +25,25 @@ public sealed class GameData
         EndTime = endTime;
     }
 
+    /// <summary>
+    /// Gets the time when the game ended.
+    /// </summary>
     public DateTime EndTime { get; }
 
+    /// <summary>
+    /// Gets the ID of the game.
+    /// </summary>
     public int GameId { get; }
 
+    /// <summary>
+    /// Gets the time when the game started.
+    /// </summary>
     public DateTime StartTime { get; }
 
+    /// <summary>
+    /// Converts the <see cref="GameData"/> into a list of strings used for HTML table row generation.
+    /// </summary>
+    /// <returns>A list of strings representing the object's properties. The list is ordered by  GameID, StartTime, and Endtime.
     public List<string> ToStringList()
     {
         return new List<string>
--- Networking/Records/PlayerData.cs
+++ Networking/Records/PlayerData.cs
@@ -6,8 +6,22 @@ using System.Globalization;
 
 namespace CS3500.Networking.Records;
 
+/// <summary>
+/// Holds the information about a player in a specific game, which includes their ID, name, highscore, 
+/// and the time they joined the game and left the game.
+/// </summary>
+/// /<remarks>This class is immutable. </remarks>
 public sealed class PlayerData
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PlayerData"/> class.
+    /// </summary>
+    /// <param name="playerId"> The ID of the player.</param>
+    /// <param name="gameId">The ID of the games this player was in.</param>
+    /// <param name="name">The display name of the player.</param>
+    /// <param name="highScore">The highest score the player earned in this game.</param>
+    /// <param name="startTime">The time the player joined the game.</param>
+    /// <param name="endTime">The time the player left the game.</param>
     public PlayerData(int playerId, int gameId, string name, int highScore, DateTime startTime, DateTime endTime)
     {
         PlayerId = playerId;
@@ -18,18 +32,40 @@ public sealed class PlayerData
         EndTime = endTime;
     }
 
+    /// <summary>
+    /// Gets the time when the player left the game.
+    /// </summary>
     public DateTime EndTime { get; }
 
+    /// <summary>
+    /// Gets the ID of the game that this player belongs to.
+    /// </summary>
     public int GameId { get; }
 
+    /// <summary>
+    /// Gets the high score the player earned during this game.
+    /// </summary>
     public int HighScore { get; }
 
+    /// <summary>
+    /// Gets the name of the player.
+    /// </summary>
     public string Name { get; }
 
+    /// <summary>
+    /// Gets the ID of the the player.
+    /// </summary>
     public int PlayerId { get; }
 
+    /// <summary>
+    /// Gets the time when the player joined the game.
+    /// </summary>
     public DateTime StartTime { get; }
 
+    /// <summary>
+    /// Converts the <see cref="PlayerData"/> into a list of strings used for HTML table row generation.
+    /// </summary>
+    /// <returns>A list of strings in the order of PlayerId, Name, StartTime, EndTime, and Dates.</returns>
     public List<string> ToStringList()
     {
         return new List<string>
